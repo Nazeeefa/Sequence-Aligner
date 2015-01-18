dc75a4998072fbde3649ed5b852ae9d4176ae99c
talibh <talib@softwarestudio.co.uk>
2015-01-18 02:28:29 +0000

    Stats Example Updated with Simple Colours

--- js/example_sastats.js
+++ js/example_sastats.js
@@ -53,13 +53,14 @@ for (i = 0; i < sequences.length; i++) {
     inptxt = inptxt.substr(secondg);
 }
 //prepare SVG
-var svgContainer = d3.select("#svgcontainer").append("svg").attr("width", (maxseqlength*10)+40).attr("height", (sequences_list.length*20)+40);
-for(i=0;i<maxseqlength;i++)
+var svgContainer = d3.select("#svgcontainer").append("svg").attr("width", (maxseqlength*15)+40).attr("height", (sequences_list.length*20)+40);
+for(i=0;i<=maxseqlength;i++)
 {
-	svgContainer.append("circle").attr('cx',20+(i*10)).attr('cy',20).attr('fill','#ccc').attr('r',3);
+	svgContainer.append("circle").attr('data-col','c'+i).attr('data-row','rh').attr('class','r0c'+i).attr('cx',20+(i*15)).attr('cy',20).attr('fill','#ccc').attr('fill-opacity','1.0').attr('r',4);
 }
 //End of SVG Test
 $("#seqoutput").html("");
+var svgarr = [];
 for (i = 0; i < sequences_list.length; i++) {
     var newtitle= sequences_list[i].title;
     diff_in_length=maxlength-sequences_list[i].titlelength;
@@ -70,7 +71,15 @@ for (i = 0; i < sequences_list.length; i++) {
         }
     }
 	//SVG Added
-		svgContainer.append("circle").attr('cx',20).attr('cy',20+(i*10)).attr('r',2);
+		svgContainer.append("circle").attr('data-col','c0').attr('data-row','r'+i+1).attr('cx',20).attr('cy',20+(i*15)).attr('r',4).attr('fill','blue');
+		for(k=0;k<sequences_list[i].seqlength;k++)
+		{
+			var svgc= svgContainer.append("text").attr('data-col','c'+(k+1)).attr('data-row','r'+(i+1)).attr('x',33+(k*15)).attr('y',40+(i*15)).attr('fill','green').text(sequences_list[i].sequence[k]);
+			if(sequences_list[i].sequence[k]=='-')
+			{
+				svgc.attr('fill','red');
+			}
+		}
 	//SVG End
     if(sequences_list[i].seqlength>0){
         characterlength=sequences_list[i].seqlength;
@@ -81,9 +90,13 @@ for (i = 0; i < sequences_list.length; i++) {
         var outtxt = $("#seqoutput").html();
         $("#seqoutput").html(outtxt + newtitle + sequences_list[i].sequence + "<br />");
         nazeefa++;
-    }
-    
-        
+		svgarr.push(
+		{
+			title:newtitle,
+			seq:sequences_list[i].sequence,
+			seqlength:sequences_list[i].sequence
+		});
+    }	
     
     //debug
     debughtml += "S:" + i +
@@ -91,6 +104,38 @@ for (i = 0; i < sequences_list.length; i++) {
         ",CleanNoSpace:" + sequences_list[i].seqlength + ")" +
         "<br/>";
 }
+//Get colour range for 1-100;
+var percentagecolour=d3.scale.linear().domain([0,50,100]).range(["green","orange","red"]);
+//update column headers
+for (i=0;i<maxseqlength;i++)
+	{
+		//for each column set to 0%
+		totalhyphen=0;
+		for(j=0;j<svgarr.length;j++)
+		{
+			if(svgarr[j].seq[i]=='-')
+			{
+				totalhyphen++;
+			}
+		}
+		var percentagehyphen = Math.round((totalhyphen/svgarr.length)*100);
+		$('circle.r0c'+(i+1)).attr('fill',percentagecolour(percentagehyphen)).attr('data-num',totalhyphen).attr('data-perc',percentagehyphen);
+		$('circle.r0c'+(i+1)).on('click',function(){
+			currentopacity = parseFloat($(this).attr('fill-opacity'));
+			if(currentopacity==1.0){
+				$(this).attr('fill-opacity','0.5');
+				$(this).attr('stroke','black');
+				$(this).attr('stroke-width','2');
+			}
+			else{
+				$(this).attr('fill-opacity','1.0');
+				$(this).attr('stroke','');
+				$(this).attr('stroke-width','0');
+			}
+		});
+		//console.log('circle.r0c'+(i+1)+": "+totalhyphen);
+	}	
+
 $("#debug").html(debughtml);
 sttxt += ", Sequences:" + sequences.length;
 sttxt += ", Processed:" + nazeefa + ", MaxLength:" + maxlength;
